f6d4b0a4da3aea0c4ac592eb64a8c9c274fd3994
r4n3n122 <renan.santos@walljobs.com.br>
2023-05-13 17:08:56 -0300

    first commit

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Avatar from '../img/perfil_portifolio.jpeg'
+import '../styles/components/sidebar.sass'
+
+export const Sidebar = () => {
+  return (
+    <aside id="sidebar">
+      <img src={Avatar} alt="Renan Santos" />
+      <p className="title">Desenvolvedor</p>
+      <p>redes sociais</p>
+      <p>informações de contato</p>
+      <a href="" className='btn'>Download currículo</a>
+    </aside>
+    )
+}
